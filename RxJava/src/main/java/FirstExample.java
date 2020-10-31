ad1f474e99f78a03fd5e74d916cca4d1e9a22cb5
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-10-31 12:07:12 +0900

    <Create> isDisposed() 함수 활용

---
+++ RxJava/src/main/java/FirstExample.java
@@ -0,0 +1,24 @@
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.Disposable;
+
+public class FirstExample {
+    /*public void emit(){
+        Observable.just("Hello!!", "RxJava 2 !!")
+                .subscribe(System.out::println);
+    }*/
+    public void emit(){
+        Observable<String> source = Observable.just("RED", "GREEN", "YELLOW");
+
+        Disposable d = source.subscribe(
+                v -> System.out.println("onNext() : value : " + v),
+                err -> System.out.println("onError() : err : " + err.getMessage()),
+                () -> System.out.println("onComplete()")
+        );
+        System.out.println("isDisposed() : " + d.isDisposed());
+    }
+
+    public static void main(String[] args) {
+        FirstExample demo = new FirstExample();
+        demo.emit();
+    }
+}
