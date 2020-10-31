475fe168e3e883a89d9cf762c435bddd11eb5b5f
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-01 08:45:48 +0900

    <Create_Kotlin> FromArray() 함수

---
+++ RxJava/src/main/java/ObservableFrommArrayKotlin.kt
@@ -0,0 +1,14 @@
+import io.reactivex.rxjava3.core.Observable
+
+class ObservableFrommArrayKotlin {
+    fun integerArray() {
+        val arr = arrayOf(100,200,300)
+
+        val source = Observable.fromArray(*arr)
+        source.subscribe(System.out::println)
+    }
+}
+fun main(){
+    val demo = ObservableFrommArrayKotlin()
+    demo.integerArray()
+}
