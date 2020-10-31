3e5c1ae6db0eed0e5459d7287bba07f6d23e1854
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-10-31 19:31:58 +0900

    <Create_Kotlin> isDisposed() 함수 사용

--- RxJava/src/main/java/FirstExampleK.kt
+++ RxJava/src/main/java/FirstExampleK.kt
@@ -1,13 +1,24 @@
 import io.reactivex.rxjava3.core.Observable
 
 class FirstExampleK {
-    fun emitKt() {
+    /*fun emitKt() {
         Observable.just(1, 2, 3, 4, 5, 6)
                 .subscribe(System.out::println)
+    }*/
+    fun emit(){
+        val source = Observable.just("RED","GREEM","BLUE")
+
+        val d = source.subscribe(
+                { v -> println("onNext() : value : $v")},
+                { err -> println("onError() : err : ${err.message}") },
+                { println("onComplete()")}
+        )
+
+        println("isDisPosed() : " + d.isDisposed)
     }
 }
 
 fun main() {
     val demo = FirstExampleK()
-    demo.emitKt()
+    demo.emit()
 }
