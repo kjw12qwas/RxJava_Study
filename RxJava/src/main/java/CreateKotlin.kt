4c935b743bdd9da6bec7dd5283a5de39634ae58c
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-01 08:35:35 +0900

    <Create_Kotlin> Create() 함수 사용

---
+++ RxJava/src/main/java/CreateKotlin.kt
@@ -0,0 +1,18 @@
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableEmitter
+
+class CreateKotlin {
+    fun emit() {
+        val source = Observable.create{ emitter: ObservableEmitter<Int> ->
+            emitter.onNext(100);
+            emitter.onNext(200)
+            emitter.onNext(300)
+            emitter.onComplete()
+        }
+        source.subscribe(System.out::println)
+    }
+}
+fun main(){
+    val demo = CreateKotlin()
+    demo.emit()
+}
