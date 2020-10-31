077c63e681311230be688b34aae18514d63594cf
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-01 08:42:07 +0900

    <Create_Java> FromArray() 함수

---
+++ RxJava/src/main/java/ObservableFromArrayJava.java
@@ -0,0 +1,14 @@
+import io.reactivex.rxjava3.core.Observable;
+
+public class ObservableFromArrayJava {
+    public void integerArray() {
+        Integer[] arr = {100,200,300};
+        Observable<Integer> source = Observable.fromArray(arr);
+        source.subscribe(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        ObservableFromArrayJava demo  = new ObservableFromArrayJava();
+        demo.integerArray();
+    }
+}
