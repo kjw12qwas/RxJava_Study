6cf0e8a25ab679a8cfa59072e0ccf5cc96dd7f2c
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-03 18:07:10 +0900

    <Create_Kotlin> subscribe() 함수

---
+++ RxJava/src/main/java/Subscribe.java
@@ -0,0 +1,48 @@
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.functions.Consumer;
+
+public class Subscribe {
+    public void emit() {
+        // subscribe()를 호출하지 않은 create()
+        /*Observable<Integer> source = Observable.create(
+                (ObservableEmitter<Integer> emitter) -> {
+                    emitter.onNext(100);
+                    emitter.onNext(200);
+                    emitter.onNext(300);
+                    emitter.onComplete();
+                }
+        );*/
+        // subscribe() 함수 변경
+        /*Observable<Integer> source = Observable.create(
+                (ObservableEmitter<Integer> emitter) -> {
+                    emitter.onNext(100);
+                    emitter.onNext(200);
+                    emitter.onNext(300);
+                    emitter.onComplete();
+                }
+        );
+
+        // 익명 객체 활용 얘
+        source.subscribe(data -> System.out.println("Result : " + data));*/
+        Observable<Integer> source = Observable.create((ObservableEmitter<Integer> emitter) -> {
+                    emitter.onNext(100);
+                    emitter.onNext(200);
+                    emitter.onNext(300);
+                    emitter.onComplete();
+                }
+        );
+        source.subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer data) throws Exception {
+                System.out.println("Result : " + data);
+            }
+        });
+
+    }
+
+    public static void main(String[] args) {
+        Subscribe demo = new Subscribe();
+        demo.emit();
+    }
+}
