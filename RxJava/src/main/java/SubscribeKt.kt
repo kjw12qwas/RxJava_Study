80ef9c83d1a873c8ccdd565d4cfd12ec77bc709e
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-03 18:20:24 +0900

    <Create_Kotlin> subscribe() 함수

---
+++ RxJava/src/main/java/SubscribeKt.kt
@@ -0,0 +1,28 @@
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableEmitter
+import io.reactivex.rxjava3.functions.Consumer
+
+class SubscribeKt (){
+    fun demo() {/*
+        val source = Observable.create { emitter : ObservableEmitter<Int> ->
+            emitter.onNext(100)
+            emitter.onNext(200)
+            emitter.onNext(300)
+            emitter.onComplete()
+        }
+        source.subscribe { data -> println("Result :  $data")}*/
+
+        val source = Observable.create { emitter : ObservableEmitter<Int> ->
+            emitter.onNext(100)
+            emitter.onNext(200)
+            emitter.onNext(300)
+            emitter.onComplete()
+        }
+        source.subscribe(Consumer<Int> { data -> println("Result : $data") })
+    }
+}
+
+fun main() {
+    val rest = SubscribeKt()
+    rest.demo()
+}
