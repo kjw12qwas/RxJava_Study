6fe94d1ca6d89c0e50f250712909eac3afaf4ca0
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-15 02:49:10 +0900

    <Create_Java> FromIterable() 함수 Order Class

---
+++ RxJava/src/main/java/Order.java
@@ -0,0 +1,16 @@
+public class Order {
+    private String mId;
+
+    public Order(String id) {
+        mId = id;
+    }
+
+    public String getId(){
+        return mId;
+    }
+
+    @Override
+    public String toString() {
+        return "Order ID: " + mId;
+    }
+}
