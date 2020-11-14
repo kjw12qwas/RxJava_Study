1d8f4111367452ab41020ec2ace057f6a5d61ae0
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-15 02:49:47 +0900

    <Create_Kotlin> FromIterable() Set,BlockingQueue

--- RxJava/src/main/java/FromlterableKt.kt
+++ RxJava/src/main/java/FromlterableKt.kt
@@ -1,4 +1,7 @@
 import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.BlockingDeque
+import java.util.concurrent.BlockingQueue
 
 class FromlterableKt {
     fun listExample() {
@@ -9,6 +12,24 @@ class FromlterableKt {
 
         val source = Observable.fromIterable(names)
         source.subscribe(System.out::println)
+    }
+    fun setExample() {
+        val cities : MutableSet<String> = HashSet()
+        cities.add("Seoul")
+        cities.add("London")
+        cities.add("Paris")
+
+        val source1 = Observable.fromIterable(cities)
+        source1.subscribe(System.out::println)
+    }
+    fun blockingQueueExample() {
+        val orderQueue : BlockingQueue<Order> = ArrayBlockingQueue(100)
+        orderQueue.add(Order("ORD-1"))
+        orderQueue.add(Order("ORD-2"))
+        orderQueue.add(Order("ORD-3"))
+
+        val source = Observable.fromIterable(orderQueue)
+        source.subscribe { order -> println(order.getId())}
     }
 }
 
