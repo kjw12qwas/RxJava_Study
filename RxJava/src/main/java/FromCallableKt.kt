6df007c49463953c2fb0746b1c7ee71b8fe6a8c8
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-16 21:46:36 +0900

    <Create_Kotlin> FromCallable()

---
+++ RxJava/src/main/java/FromCallableKt.kt
@@ -0,0 +1,18 @@
+import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.Callable
+
+class FromCallableKt{
+    fun emit() {
+        val callable = Callable {
+            Thread.sleep(1000)
+            "Hello Callable"
+        }
+        val source = Observable.fromCallable(callable)
+        source.subscribe(System.out::println)
+    }
+}
+
+fun main() {
+    val demo = FromCallableKt()
+    demo.emit()
+}
