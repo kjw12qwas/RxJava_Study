58112160f3ea15b33c2e0fe29cce9ac6fd03d21a
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-16 21:55:59 +0900

    <Create_Kotlin> FromFuture()

---
+++ RxJava/src/main/java/FromFutureKt.kt
@@ -0,0 +1,19 @@
+import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.Executors
+
+class FromFutureKt {
+    fun emit(){
+        val future = Executors.newSingleThreadExecutor().submit<String>{
+            Thread.sleep(1000)
+            "Hello Future"
+        }
+
+        val source = Observable.fromFuture(future)
+        source.subscribe(System.out::println)
+    }
+}
+
+fun main() {
+    val demo = FromFutureKt()
+    demo.emit()
+}
