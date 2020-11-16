3979a06ac86003c621a011676b7062631ba400f8
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-16 21:56:10 +0900

    <Create_Java> FromFuture()

---
+++ RxJava/src/main/java/FromFutureJava.java
@@ -0,0 +1,21 @@
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FromFutureJava {
+    public void emit(){
+        Future<String> future = Executors.newSingleThreadExecutor().submit(() -> {
+            Thread.sleep(1000);
+            return "Hello future";
+        });
+
+        Observable<String> source = Observable.fromFuture(future);
+        source.subscribe(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        FromFutureJava demo = new FromFutureJava();
+        demo.emit();
+    }
+}
