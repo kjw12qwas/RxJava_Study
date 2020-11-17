b2af1c9560521e6b05409a1f442f9a91cd705123
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-17 21:49:37 +0900

    <Create_Java> Observable 클래스에서 Single 클래스 사용 예

--- RxJava/src/main/java/Single/SingleJustJava.java
+++ RxJava/src/main/java/Single/SingleJustJava.java
@@ -1,11 +1,39 @@
 package Single;
 
+import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Single;
 
 public class SingleJustJava {
     public void emit(){
-        Single<String> source = Single.just("Hello Single");
-        source.subscribe(System.out::println);
+//        Single<String> source = Single.just("Hello Single");
+//        source.subscribe(System.out::println);
+
+        // 1. 기존 Observable에서 Single 객체로 변환하기
+        Observable<String> source = Observable.just("Hello Single");
+        Single.fromObservable(source)
+                .subscribe(System.out::println);
+
+        // 2. Single() 함수를 호출해 Single 객체 생성하기
+        Observable.just("Hello Single")
+                .single("default item")
+                .subscribe(System.out::println);
+
+        // 3. first() 함수를 호출해 Single 객체 생성하기
+        String[] colors = {"Red", "Blue", " Gold"};
+        Observable.fromArray(colors)
+                .first("default value")
+                .subscribe(System.out::println);
+
+        // 4. empty Observable에서 Single 객체 생성하기
+        Observable.empty()
+                .single("default value")
+                .subscribe(System.out::println);
+
+        // 5. take() 함수에서 Single 객체 생성하기
+        Observable.just(new Order("ORD-1"), new Order("ORD-2"))
+                .take(1)
+                .single(new Order("default order"))
+                .subscribe(System.out::println);
     }
 
     public static void main(String[] args) {
