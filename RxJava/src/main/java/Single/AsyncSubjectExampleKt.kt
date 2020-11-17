93e38e63095105e1a529a2ad8339c7ebb148b18e
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-17 22:31:36 +0900

    <Create_Kotlin> AsyncSubject Class 사용 예

---
+++ RxJava/src/main/java/Single/AsyncSubjectExampleKt.kt
@@ -0,0 +1,20 @@
+package Single
+
+import io.reactivex.rxjava3.subjects.AsyncSubject
+
+class AsyncSubjectExampleKt{
+    fun emit(){
+        val subject = AsyncSubject.create<String>()
+        subject.subscribe { data -> println("Subscriber #1 => $data") }
+        subject.onNext("1")
+        subject.onNext("3")
+        subject.subscribe { data -> println("Subscriber #2 => $data")}
+        subject.onNext("5")
+        subject.onComplete()
+    }
+}
+
+fun main() {
+    val demo = AsyncSubjectExampleKt()
+    demo.emit()
+}
