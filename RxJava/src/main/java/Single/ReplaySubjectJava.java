546c170745419eee5d6bf5451654cdbd08972f57
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-19 21:22:41 +0900

    <Create_Java> ReplaySubject Class 사용 예

---
+++ RxJava/src/main/java/Single/ReplaySubjectJava.java
@@ -0,0 +1,20 @@
+package Single;
+
+import io.reactivex.rxjava3.subjects.ReplaySubject;
+
+public class ReplaySubjectJava {
+    public void emit(){
+        ReplaySubject<String> subject = ReplaySubject.create();
+        subject.subscribe(data -> System.out.println("Subscriber #1 => " + data));
+        subject.onNext("1");
+        subject.onNext("3");
+        subject.subscribe(data -> System.out.println("Subscriber #1 => " + data));
+        subject.onNext("5");
+        subject.onComplete();
+    }
+
+    public static void main(String[] args) {
+        ReplaySubjectJava demo = new ReplaySubjectJava();
+        demo.emit();
+    }
+}
